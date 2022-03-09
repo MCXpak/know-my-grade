69d38b9bb29ba1a2c98d6562520086d994e19294
Mourad Lasga Terceras <mouradlasga@gmail.com>
2022-03-09 00:08:48 +0000

    grades now round to 2dp, better variable names

--- script.js
+++ script.js
@@ -1,72 +1,59 @@
-const grInputs = ["grInput1"];
+let gradeInputCounter = 1;
 
-let grInputCounter = 1;
 
-function addGradeInputs(parentGrInput){
-    grInputCounter += 1;
+function addGradeInputs(previousGradeInput){ //function to create new grade input
+    gradeInputCounter += 1;
 
-    let parent = document.getElementById(parentGrInput);
-    var ok = true;
-    if (ok === true) {
-        var gradeDiv = document.createElement("div");
-        gradeDiv.className = "grade-div";
+    let parent = document.getElementById(previousGradeInput); 
+    
+    var gradeDiv = document.createElement("div");
+    gradeDiv.className = "grade-div";
 
-        const grInput = "grInput" + grInputCounter.toString();
-        console.log("Next: " + grInput)
+    const gradeInput = "gradeInput" + gradeInputCounter.toString();
+    console.log("Next: " + gradeInput)
 
-        var inputs = document.createElement("div");
-        inputs.className = "inputs"
+    var inputs = document.createElement("div");
+    inputs.className = "inputs"
 
-        var markInput = document.createElement("input");
-        var weightInput = document.createElement("input");
-        var creditInput = document.createElement("input");
-        markInput.type = "text";
-        weightInput.type= "text";
-        creditInput.type = "text";
-        markInput.className = "grade-input";
-        weightInput.className = "weight-input";
-        creditInput.className = "grade-credit";
-        markInput.placeholder = "0-100";
-        weightInput.placeholder = "%";
-        creditInput.placeholder = "15"
+    var markInput = document.createElement("input");
+    var weightInput = document.createElement("input");
+    var creditInput = document.createElement("input");
+    markInput.type = "text";
+    weightInput.type= "text";
+    creditInput.type = "text";
+    markInput.className = "grade-input";
+    weightInput.className = "weight-input";
+    creditInput.className = "grade-credit";
+    markInput.placeholder = "0-100";
+    weightInput.placeholder = "%";
+    creditInput.placeholder = "15"
 
-        var addGradeDiv = document.createElement("div");
-        var minusGradeDiv = document.createElement("div");
-        addGradeDiv.className = "grade-button"
-        minusGradeDiv.className = "grade-button"
+    var addGradeDiv = document.createElement("div");
+    var minusGradeDiv = document.createElement("div");
+    addGradeDiv.className = "grade-button"
+    minusGradeDiv.className = "grade-button"
 
-        addGradeDiv.innerHTML = '<button class="add-grade" onclick=addGradeInputs("'+ grInput +'")><i class="fa-solid fa-plus"></i></button></div>'
-        minusGradeDiv.innerHTML = '<button class="minus-grade" onclick=removeGradeInputs("'+ grInput +'")><i class="fa-solid fa-minus"></i></button>'
-        
-        inputs.appendChild(markInput);
-        inputs.appendChild(weightInput);
-        inputs.appendChild(creditInput);
+    addGradeDiv.innerHTML = '<button class="add-grade" onclick=addGradeInputs("'+ gradeInput +'")><i class="fa-solid fa-plus"></i></button></div>'
+    minusGradeDiv.innerHTML = '<button class="minus-grade" onclick=removeGradeInputs("'+ gradeInput +'")><i class="fa-solid fa-minus"></i></button>'
+    
+    inputs.appendChild(markInput);
+    inputs.appendChild(weightInput);
+    inputs.appendChild(creditInput);
 
-        gradeDiv.appendChild(inputs);
-        gradeDiv.appendChild(addGradeDiv);
-        gradeDiv.appendChild(minusGradeDiv);
-        
-        gradeDiv.id = grInput;
-        console.log("New ID: " + gradeDiv.id)
-        
-        parent.after(gradeDiv);
-        //let gradeContainer = document.getElementById("grades");
-        //grades.insertBefore(gradeDiv, parent)
-        //insertAfter(gradeDiv, gradeContainer);
-    }
+    gradeDiv.appendChild(inputs);
+    gradeDiv.appendChild(addGradeDiv);
+    gradeDiv.appendChild(minusGradeDiv);
+    
+    gradeDiv.id = gradeInput;
+    console.log("New ID: " + gradeDiv.id)
+    
+    parent.after(gradeDiv);
+    
 };
 
-function insertAfter(newNode, existingNode) {
-    existingNode.parentNode.insertBefore(newNode, existingNode.nextSibling);
-}
-
-/*function insertAfter(referenceNode, newNode) {
-    referenceNode.previousSibling.insertBefore(newNode, referenceNode.nextSibling);
-  }*/
-
-function removeGradeInputs(grInput){
+function removeGradeInputs(gradeInput){
     //deal with all inputs disappearing after minusing them all XD
-    let div = document.getElementById(grInput)
+    let div = document.getElementById(gradeInput)
     div.remove();
 }
 
@@ -80,5 +67,6 @@ function calcGrade(){
     console.log(grades);
     const gradeSum = grades.reduce((a, b) => a+b);
     const avg = gradeSum/grades.length;
-    document.getElementById("grade-output").innerHTML = "Grade: " + avg;
+    const roundedAvg = Math.round((avg + Number.EPSILON) * 100) / 100; //round to 2dp
+    document.getElementById("grade-output").innerHTML = "Grade: " + roundedAvg;
 }
